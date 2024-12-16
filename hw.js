12a0a8d45803b3a087edf5794ca1206e8dc2bd95
QAAnton23 <ironbrunka2228@gmail.com>
2024-12-17 01:53:38 +0400

    Update hw.js

--- hw.js
+++ hw.js
@@ -1,11 +1,7 @@
-
 var text = "Often, Brain, Happiness, Look, Joyful, Wonderful";
 
-
-var regex = /\b[^Aa]{6,}\b/g;
-
+var regex = /\b(?!.*[Aa])[a-zA-Z]{6,}\b/g;
 
 var matches = text.match(regex);
-
 
 console.log(matches);
